27b93ebb077f45977664afe0e640c143bf64d398
Renan Elias Betereli <renan.bete@gmail.com>
2022-05-09 21:04:54 -0300

    versao final Clone Netflix, commit inicial de alguns exercicios do curso de Java

---
+++ Java/exercicios_DIO/src/O/O/Main.java
@@ -0,0 +1,22 @@
+package O.O;
+
+public class Main {
+    public void main(String[] args) {
+
+        double precoCombustivel = 6.54;
+
+        Carro honda = new Carro();
+        honda.modelo = "Fit";
+        honda.cor = "Preto";
+        honda.capacidadeTanque = 45;
+
+        Carro ford = new Carro();
+        ford.modelo = "Fiesta";
+        ford.cor = "Branco";
+        ford.capacidadeTanque = 52;
+
+        System.out.println(honda);
+
+    }
+}
+//994340416
